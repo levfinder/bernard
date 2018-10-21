fb35d8a55af67c8c4e3b6e42ad4b7c0b301becab
Viren Nadkarni <viren.nadkarni@gmail.com>
2018-10-21 12:31:52 +0200

    Add mock data seeder script

---
+++ scripts/seed_db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import datetime
+import uuid
+
+from django_seed import Seed
+from bernard.core.models import Vendor, Order, Vehicle, Delivery, LocationUpdate
+
+
+seeder = Seed.seeder(locale='sv_SE')
+
+seeder.add_entity(Vendor, 5, {
+    'name': lambda x: seeder.faker.company(),
+})
+
+seeder.add_entity(Vehicle, 10, {
+    'external_id': lambda x: seeder.faker.license_plate(),
+})
+
+seeder.add_entity(LocationUpdate, 50, {
+    'latitude': lambda x: seeder.faker.latitude(),
+    'latitude': lambda x: seeder.faker.longitude(),
+})
+
+seeder.add_entity(Order, 20, {
+    'external_id': lambda x: uuid.uuid1(),
+    'street': lambda x: seeder.faker.street_address(),
+    'city': lambda x: seeder.faker.city(),
+    'postcode': lambda x: seeder.faker.postcode(),
+    'phone': lambda x: seeder.faker.phone_number(),
+    'email': lambda x: seeder.faker.ascii_email(),
+})
+
+def run(*args):
+    seeder.execute()
+
