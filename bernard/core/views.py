1d2afe78945e8bbe2f8e1565faf97a704379ac74
Viren Nadkarni <viren.nadkarni@gmail.com>
2018-10-28 22:34:42 +0100

    Implement notification mgmt

--- bernard/core/views.py
+++ bernard/core/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -8,6 +9,7 @@ from bernard.core.models import LocationUpdate, Notification, Organisation, \
 from bernard.core.enums import NotificationStatusEnum
 
 import datetime
+import uuid
 
 
 def login_view(request):
@@ -41,8 +43,9 @@ def logout_view(request):
 
 @login_required
 def overview(request):
+    ctx = dict()
+
     if request.method == 'GET':
-        ctx = dict()
         ctx['settings'] = settings
 
         return render(request, 'bernard/overview.html', ctx)
@@ -50,23 +53,47 @@ def overview(request):
 
 @login_required
 def notification(request, id):
+    ctx = dict()
     if request.method == 'GET':
-        return render(request, 'bernard/notification.html')
+
+        ctx['notification'] = Notification.objects.get(id=id)
+
+        return render(request, 'bernard/notification.html', ctx)
 
 
 @login_required
 def notifications(request):
+    ctx = dict()
     if request.method == 'GET':
-        return render(request, 'bernard/notifications.html')
+
+        organisation = getattr(request.user, 'organisation', None)
+
+        if not organisation:
+            ctx['notifications'] = Notification.objects.all()
+        else:
+            ctx['notifications'] = Notification.objects.filter(
+                organisation=organisation.id)
+
+        return render(request, 'bernard/notifications.html', ctx)
 
 
 @login_required
 def notification_new(request):
+    ctx = dict()
+
     if request.method == 'GET':
-        ctx = dict()
         ctx['now'] = datetime.datetime.now().timestamp()
-        ctx['vehicles'] = Vehicle.objects.filter(
-            organisation=request.user.organisation.id)
+
+        organisation = getattr(request.user, 'organisation', None)
+
+        if not organisation:
+            # user is admin, show org field
+            ctx['vehicles'] = Vehicle.objects.all()
+            ctx['organisations'] = Organisation.objects.all()
+        else:
+            ctx['vehicles'] = Vehicle.objects.filter(
+                organisation=organisation.id)
+
         return render(request, 'bernard/notification_new.html', ctx)
     elif request.method == 'POST':
         trigger = datetime.datetime.strptime(
@@ -74,11 +101,43 @@ def notification_new(request):
         expiry = datetime.datetime.strptime(
             request.POST.get('expiry_datetime'), '%Y-%m-%d %H:%M')
 
+        ref_id = request.POST.get('ref_id')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email', '')
+
+        organisation = getattr(request.user, 'organisation', None)
+
+        if not organisation:
+            # user is admin
+            organisation = Organisation.objects.get(
+                id=int(request.POST.get('organisation', '-1')))
+
+        vehicle = Vehicle.objects.get(
+            id=int(request.POST.get('vehicle', '-1')))
+        key = str(uuid.uuid4()).replace('-', '')
+
+        Notification.objects.create(
+            ref_id=ref_id,
+            phone=phone,
+            email=email,
+            key=key,
+            trigger_datetime=trigger,
+            expiry_datetime=expiry,
+            organisation=organisation,
+            vehicle=vehicle
+        )
+
+        messages.add_message(
+            request, messages.INFO, 'Notification object created')
+
+        return redirect('notifications')
+
 
 @login_required
 def vehicle(request, id):
+    ctx = dict()
+
     if request.method == 'GET':
-        ctx = dict()
         ctx['settings'] = settings
 
         ctx['vehicle'] = Vehicle.objects.get(id=id)
@@ -91,8 +150,9 @@ def vehicle(request, id):
 
 @login_required
 def vehicles(request):
+    ctx = dict()
+
     if request.method == 'GET':
-        ctx = dict()
         ctx['settings'] = settings
 
         ctx['vehicles'] = Vehicle.objects.all()
@@ -102,17 +162,32 @@ def vehicles(request):
 
 @login_required
 def vehicle_new(request):
+    ctx = dict()
+
     if request.method == 'GET':
-        return render(request, 'bernard/vehicle_new.html')
+        organisation = getattr(request.user, 'organisation', None)
+
+        if not organisation:
+            # user is admin, show org field
+            ctx['organisations'] = Organisation.objects.all()
+
+        return render(request, 'bernard/vehicle_new.html', ctx)
+
     elif request.method == 'POST':
         ref_id = request.POST.get('ref_id')
         info = request.POST.get('info')
-        org = Organisation.objects.get(id=request.user.organisation.id)
+
+        organisation = getattr(request.user, 'organisation', None)
+
+        if not organisation:
+            # user is admin
+            organisation = Organisation.objects.get(
+                id=int(request.POST.get('organisation', '-1')))
 
         Vehicle.objects.create(
             ref_id=ref_id,
             info=info,
-            organisation=org,
+            organisation=organisation,
         )
 
         return redirect('vehicles')
@@ -134,4 +209,3 @@ def track(request):
 
         else:
             pass
-
