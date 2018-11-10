db920ca9e3d4159883985398801b2a512d5571f3
Viren Nadkarni <viren.nadkarni@gmail.com>
2018-11-10 12:31:32 +0100

    Add routing algorithm

--- bernard/dashboard/views.py
+++ bernard/dashboard/views.py
@@ -4,8 +4,11 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+from ortools.constraint_solver import pywrapcp
+
 from bernard.core.models import Driver, Stop
 
+import googlemaps
 import re
 
 
@@ -16,7 +19,7 @@ def login_view(request):
         return render(
             request, 'dashboard/login.html',
             {'next_path': next_path,
-             'is_prod': settings.LF_ENVIRONMENT == 'prod'})
+             'lf_env': settings.LF_ENVIRONMENT})
 
     elif request.method == 'POST':
         username = request.POST.get('username')
@@ -56,6 +59,8 @@ def drivers(request):
     if request.method == 'GET':
         ctx = {}
         ctx['drivers'] = Driver.objects.all()
+        ctx['googlemaps_key'] = settings.GOOGLE_MAPS_API_KEY
+
         return render(request, 'dashboard/drivers.html', ctx)
 
 
@@ -99,7 +104,10 @@ def driver(request, _id):
 def stops(request):
     if request.method == 'GET':
         ctx = {}
+
         ctx['stops'] = Stop.objects.all()
+        ctx['googlemaps_key'] = settings.GOOGLE_MAPS_API_KEY
+
         return render(request, 'dashboard/stops.html', ctx)
 
 
@@ -139,9 +147,67 @@ def stop(request, _id):
         elif _method == 'PUT':
             pass
 
+
 @login_required
 def route(request):
-    return render(request, 'dashboard/route.html')
+    gm = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+
+    addresses = [_.address for _ in Stop.objects.filter()]
+    dist_matrix = gm.distance_matrix(origins=addresses, destinations=addresses)
+
+    optimise_criteria = 'distance'
+    matrix = []
+    ctx = {}
+    ctx['googlemaps_key'] = settings.GOOGLE_MAPS_API_KEY
+
+    for element_obj in dist_matrix['rows']:
+        row = []
+        elements = element_obj['elements']
+        for item in elements:
+            row.append(item[optimise_criteria]['value'])
+        matrix.append(row)
+
+    tsp_size = len(addresses)
+    num_routes = 1
+
+    # start and end address index
+    depot = 0
+
+    def get_distance_callback(m):
+        def distance_callback(from_node, to_node):
+            return int(m[from_node][to_node])
+        return distance_callback
+
+    if tsp_size > 1:
+        route = pywrapcp.RoutingModel(tsp_size, num_routes, depot)
+
+        search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+
+        dist_callback = get_distance_callback(matrix)
+        route.SetArcCostEvaluatorOfAllVehicles(dist_callback)
+
+        assignment = route.SolveWithParameters(search_parameters)
+
+        if assignment:
+            total_distance = assignment.ObjectiveValue()
+
+            route_number = 0
+            index = route.Start(route_number)
+            path = []
+
+            while not route.IsEnd(index):
+                path.append(addresses[route.IndexToNode(index)])
+                index = assignment.Value(route.NextVar(index))
+
+            path.append(addresses[route.IndexToNode(index)])
+
+            ctx['path'] = path
+            ctx['distance'] = '{} {}'.format(
+                total_distance,
+                'm' if optimise_criteria == 'distance' else 'seconds'
+            )
+
+    return render(request, 'dashboard/route.html', ctx)
 
 
 @login_required
